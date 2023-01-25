08e053887479b4facf63421e1fea7aec085661be
Olenka Sanchez <olesa600@gmail.com>
2023-01-24 21:29:15 -0500

    ADD: import typography font and size

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import { Global } from '@emotion/react';
 import { AuthProvider } from './context/auth-context';
+import { global, reset } from './styles';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Global styles={reset} />
-    <Global styles={global} /> */}
+    <Global styles={reset} />
+    <Global styles={global} />
     <BrowserRouter>
       <AuthProvider>
         <App />
