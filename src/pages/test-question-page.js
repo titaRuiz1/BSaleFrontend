3b99d8f73694fe24e687670a1644e207bd161031
Olenka Sanchez <olesa600@gmail.com>
2023-02-02 16:53:01 -0500

    WIP: make dinamic the number of pages and make the handleSubmit and runTest functions

--- src/pages/test-question-page.js
+++ src/pages/test-question-page.js
@@ -41,26 +41,54 @@ const Text2 = styled.p`
 
 function TestQuestionPage() {
   const navigate = useNavigate();
-  const { position } = useAuth();
+  const { position, sumCorrectAnswer, setSumCorrectAnswer, mulChoiceQuestions } = useAuth();
+  const [currentTestQuestion, setCurrentTestQuestion] = useState(0);
   const [code, setCode] = useState(
     `function myFunction(param1, param2) {\n  //happy coding\n\n\n}`
   );
   const [test1Status, setTest1Status] = useState(null);
   const [test2Status, setTest2Status] = useState(true);
   const [test3Status, setTest3Status] = useState(false);
-  const [test4Status, setTest4Status]= useState(false);
+  const [test4Status, setTest4Status] = useState(false);
 
+  // const [testStatus, setTestStatus] = useState([null,null,null,null])
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (test1Status && test2Status && test3Status && test4Status) setSumCorrectAnswer(sumCorrectAnswer + 1);
+    navigate("/results")
+
+    // CUANDO TENGA TODOS LOS QUESTIONS
+    //  if (currentTestQuestion < testQuestions.length - 1) {
+    // if (currentTestQuestion < 5) {
+    //   setCurrentTestQuestion(currentTestQuestion + 1)
+    // } else {
+    //   navigate("/results")
+    // }
+  }
+
+  console.log(code)
+
+  function runTests(event) {
+    event.preventDefault();
+    let test1 = eval(`(${code})`);
+    let result1 = test1(3, 5)
+    // if(result1 === testQuestion.output) setTest1Status(true)
+
+    console.log('resutado', result1)
+  }
 
   return (
-    <Wrapper1 style={{alignItems:"center", justifyContent:"center"}}>
+    <Wrapper1 style={{ alignItems: "center", justifyContent: "center" }}>
       <Navbar />
       <Wrapper1 style={{ maxWidth: "868px", gap: "37px", marginTop: "48px" }}>
-        <Wrapper1 style={{gap: "32px"}}>
+        <Wrapper1 style={{ gap: "32px" }}>
           <Text1>{position.title}</Text1>
-          <Text1>Pregunta 6 de 10</Text1>
+          <Text1>Pregunta {currentTestQuestion + mulChoiceQuestions?.length + 1} de 10</Text1>
         </Wrapper1>
+        {/* {testQuestions[currentTestQuestion].question.description} */}
         <Text2>Eget mollis mauris vivamus eget cursus tincidunt mauris nisi. Adipiscing sit dolor blandit et mattis. Sagittis non ultrices viverra non ac tempor. Posuere felis at ultricies purus libero diam. Non non urna tellus vehicula auctor ut massa malesuada. Nulla fermentum in donec mi maecenas iaculis amet mauris est.</Text2>
-        <Wrapper1 style={{height:"270px", alignItems:"center", justifyContent:"center", padding:"0px 30px"}}>
+        <Wrapper1 style={{ height: "270px", alignItems: "center", justifyContent: "center", padding: "0px 30px" }}>
           <Editor
             language="javascript"
             theme="vs-dark"
@@ -71,50 +99,45 @@ function TestQuestionPage() {
           />
         </Wrapper1>
         <Wrapper1 style={{ justifyContent: "center", alignItems: "center" }}>
-          <Button
-            width="71px"
-            >
-            Test
-          </Button>
+          <Button width="71px" onClick={runTests}>Test</Button>
         </Wrapper1>
         <TestsContainer>
-            {test1Status === null?
-             <Option padding={`0px 19px`} border={`none`} id={`answer1`} background={`${colors.white}`} label={`Descripcion de Opcion`} />
+          {/* {testQuestions[currentTestQuestion].tests && testQuestions[currentTestQuestion].tests.map((option, idx )=> { */}
+          {test1Status === null ?
+            <Option padding={`0px 19px`} border={`none`} id={`answer1`} value={`answer1`} background={`${colors.white}`} label={`Descripcion de Opcion`} />
+            :
+            test1Status === true ?
+              <Option padding={`0px 19px`} border={`none`} id={`answer1`} value={`answer1`} background={`${colors.green}`} label={`Descripcion de Opcion`} />
               :
-             test1Status === true?
-              <Option padding={`0px 19px`} border={`none`} id={`answer1`} background={`${colors.green}`} label={`Descripcion de Opcion`} />
+              <Option padding={`0px 19px`} border={`none`} id={`answer1`} value={`answer1`} background={`${colors.red}`} label={`Descripcion de Opcion`} />
+          }
+          {test2Status === null ?
+            <Option padding={`0px 19px`} border={`none`} id={`answer2`} value={`answer2`} background={`${colors.white}`} label={`Descripcion de Opcion`} />
+            :
+            test2Status === true ?
+              <Option padding={`0px 19px`} border={`none`} id={`answer2`} value={`answer2`} background={`${colors.green}`} label={`Descripcion de Opcion`} />
               :
-              <Option padding={`0px 19px`} border={`none`} id={`answer1`} background={`${colors.red}`} label={`Descripcion de Opcion`} />
-            }
-            {test2Status === null?
-              <Option padding={`0px 19px`} border={`none`} id={`answer2`} background={`${colors.white}`} label={`Descripcion de Opcion`} />
+              <Option padding={`0px 19px`} border={`none`} id={`answer2`} value={`answer2`} background={`${colors.red}`} label={`Descripcion de Opcion`} />
+          }
+          {test3Status === null ?
+            <Option padding={`0px 19px`} border={`none`} id={`answer3`} value={`answer3`} background={`${colors.white}`} label={`Descripcion de Opcion`} />
+            :
+            test3Status === true ?
+              <Option padding={`0px 19px`} border={`none`} id={`answer3`} value={`answer3`} background={`${colors.green}`} label={`Descripcion de Opcion`} />
               :
-             test2Status === true?
-              <Option padding={`0px 19px`} border={`none`} id={`answer2`} background={`${colors.green}`} label={`Descripcion de Opcion`} />
+              <Option padding={`0px 19px`} border={`none`} id={`answer3`} value={`answer3`} background={`${colors.red}`} label={`Descripcion de Opcion`} />
+          }
+          {test4Status === null ?
+            <Option padding={`0px 19px`} border={`none`} id={`answer4`} value={`answer4`} background={`${colors.white}`} label={`Descripcion de Opcion`} />
+            :
+            test4Status === true ?
+              <Option padding={`0px 19px`} border={`none`} id={`answer4`} value={`answer4`} background={`${colors.green}`} label={`Descripcion de Opcion`} />
               :
-              <Option padding={`0px 19px`} border={`none`} id={`answer2`} background={`${colors.red}`} label={`Descripcion de Opcion`} />
-            }
-            {test3Status === null?
-              <Option padding={`0px 19px`} border={`none`} id={`answer3`} background={`${colors.white}`} label={`Descripcion de Opcion`} />
-              :
-             test3Status === true?
-              <Option padding={`0px 19px`} border={`none`} id={`answer3`} background={`${colors.green}`} label={`Descripcion de Opcion`} />
-              :
-              <Option padding={`0px 19px`} border={`none`} id={`answer3`} background={`${colors.red}`} label={`Descripcion de Opcion`} />
-            }
-            {test4Status === null?
-              <Option padding={`0px 19px`} border={`none`} id={`answer4`} background={`${colors.white}`} label={`Descripcion de Opcion`} />
-              :
-             test4Status === true?
-              <Option padding={`0px 19px`} border={`none`} id={`answer4`} background={`${colors.green}`} label={`Descripcion de Opcion`} />
-              :
-              <Option padding={`0px 19px`} border={`none`} id={`answer4`} background={`${colors.red}`} label={`Descripcion de Opcion`} />
-            }
+              <Option padding={`0px 19px`} border={`none`} id={`answer4`} value={`answer4`} background={`${colors.red}`} label={`Descripcion de Opcion`} />
+          }
         </TestsContainer>
-        <Wrapper1  style={{justifyContent:"center", alignItems:"center"}}>
-          <Button
-            width="90px"
-            onClick={() => navigate("/results")}>
+        <Wrapper1 style={{ justifyContent: "center", alignItems: "center" }}>
+          <Button width="90px" onClick={handleSubmit}>
             Enviar
           </Button>
         </Wrapper1>
