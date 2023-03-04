39a819658d84fde30ab4622484dbc936a79b25b4
Tita Ruiz <asuntita.rl@gmail.com>
2023-03-03 19:23:53 -0500

    config deploy railway

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export const tokenKey = "bsale_token";
-export const BASE_URI = "http://127.0.0.1:3000/"
+export const BASE_URI = "https://bsaleproject-production.up.railway.app/"
+// export const BASE_URI = "http://127.0.0.1:3000/"
 
 
 
